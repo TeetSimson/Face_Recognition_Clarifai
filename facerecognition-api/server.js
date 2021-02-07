cca2fbcf767d57a1f8a59ecb04aa908f13c78497
Teet Simson <teetsimson@Teets-MacBook-Pro.local>
2021-02-08 01:43:30 +0200

    added port

--- facerecognition-api/server.js
+++ facerecognition-api/server.js
@@ -54,6 +54,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 // Server starts listening
-app.listen(3000, () => {
-    console.log("App is running on port 3000")
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`)
 })
