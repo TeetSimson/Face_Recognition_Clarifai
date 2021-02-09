a1e01289303cec99446ac70f3749669e74514831
Teet Simson <teetsimson@Teets-MacBook-Pro.local>
2021-02-09 16:32:05 +0200

    database ssl

--- facerecognition-api/server.js
+++ facerecognition-api/server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-shaped-24063',
-      user : 'teetsimson',
-      password : '',
-      database : 'faceRecognitionDB'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
